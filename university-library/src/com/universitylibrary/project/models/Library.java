91b279da969d232ebf30657912f7d93aae052607
Ángeles Rondón <angelesemilianaa@gmail.com>
2026-09-19 17:15:50 -0500

    Create Library object
    * This is the main object for the project.

---
+++ university-library/src/com/universitylibrary/project/models/Library.java
@@ -0,0 +1,95 @@
+package com.universitylibrary.project.models;
+
+import java.util.ArrayList;
+
+public class Library {
+  private String name;
+  private String address;
+  private String schedule;
+  private double penaltyPercentage; // This attribute may change
+  private ArrayList<Users> userList;
+  private ArrayList<Book> bookList;
+
+  private Library(Builder builder) {
+   this.name = builder.name;
+   this.address = builder.address;
+   this.schedule = builder.schedule;
+   this.penaltyPercentage = builder.penaltyPercentage;
+   this.bookList = builder.bookList;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getSchedule() {
+    return schedule;
+  }
+
+  public double getPenaltyPercentage() {
+    return penaltyPercentage;
+  }
+
+  public ArrayList<User> getUserList() {
+    return userList;
+  }
+
+  public ArrayList<Book> getBookList() {
+    return bookList;
+  }
+
+  public void changePenaltyValue (double newPenaltyValue) {
+    this.penaltyPercentage = newPenaltyValue;
+  }
+
+  public void addBook (Book book) {
+    this.bookList.add(book) ;
+  }
+
+  public static class Builder {
+    private String name;
+    private String address;
+    private String schedule;
+    private double penaltyPercentage = 20.000;
+    private ArrayList<Users> userList;
+    private ArrayList<Book> bookList;
+
+    public Builder name(String name) {
+      this.name = name;
+      return this;
+    }
+
+    public Builder address() {
+      this.address = address;
+      return this;
+    }
+
+    public Builder schedule() {
+      this.schedule = schedule;
+      return this;
+    }
+
+    public Builder penaltyPercentage() {
+      this.penaltyPercentage = penaltyPercentage;
+      return this;
+    }
+
+    public Builder userList() {
+      this.userList = userList;
+      return this;
+    }
+
+    public Builder bookList() {
+      this.bookList = bookList;
+      return this;
+    }
+
+    public Library build() {
+      return new Library(this);
+    }
+  }
+}
